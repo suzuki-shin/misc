90d391feb3d16545f88927273974bd0513f3fb0b
Shinichiro Suzuki <shinichiro.su@gmail.com>
2012-03-01 18:00:17 +0900

    一応DLできるようになった

--- convertjs/coffee/convert.coffee
+++ convertjs/coffee/convert.coffee
@@ -1,6 +1,51 @@
-@ftoh = (str) ->
+glbal = this
+
+glbal.ftoh = (str) ->
   (fullchar2halfchar(c) for c in str).join('')
 
 fullchar2halfchar = (char) ->
   if @table[char]? then @table[char] else char
-#   char.replace(/[ａ-Ｚ]/, (c) -> "@")
+
+# file api
+selectFile = (ev) ->
+  file = ev.target.files[0]
+  alert file.name + ' is selected!'
+
+  reader = new FileReader()
+  reader.readAsText(file)
+
+  reader.onload = (ev) ->
+    console.log 'readeronload'
+    textData = reader.result
+    alert textData
+    alert glbal.ftoh(textData)
+    console.log textData.split("\n")
+    file_name = (file.name.match /^(\w+)/)[0]
+    file_name or= 'xxxxx'
+    console.log file_name
+    $('#download-link').attr('href', "data:application/octet-stream," + encodeURIComponent(reader.result))
+    $('#download-link').show()
+#     db.transaction (tx) -> saveIfNotExists(tx, file_name, textData)
+
+  reader.onerror = (ev) ->
+    alert 'error'
+
+
+outPut = (ev) ->
+  file = ev.target.files[0]
+  alert file.name + ' is selected!'
+
+  reader = new FileReader()
+  reader.readAsText(file)
+
+  reader.onload = (ev) ->
+    @.attr('href', "data:application/octet-stream," + encodeURIComponent(reader.result))
+
+
+###
+# event
+###
+$ ->
+  $(document).on 'change', '#selectFile', selectFile
+
+  $(document).on 'click', '#download-link', outPut
--- convertjs/js/convert.js
+++ convertjs/js/convert.js
@@ -1,7 +1,9 @@
 (function() {
-  var fullchar2halfchar;
+  var fullchar2halfchar, glbal, outPut, selectFile;
 
-  this.ftoh = function(str) {
+  glbal = this;
+
+  glbal.ftoh = function(str) {
     var c;
     return ((function() {
       var _i, _len, _results;
@@ -21,5 +23,49 @@
       return char;
     }
   };
+
+  selectFile = function(ev) {
+    var file, reader;
+    file = ev.target.files[0];
+    alert(file.name + ' is selected!');
+    reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function(ev) {
+      var file_name, textData;
+      console.log('readeronload');
+      textData = reader.result;
+      alert(textData);
+      alert(glbal.ftoh(textData));
+      console.log(textData.split("\n"));
+      file_name = (file.name.match(/^(\w+)/))[0];
+      file_name || (file_name = 'xxxxx');
+      console.log(file_name);
+      $('#download-link').attr('href', "data:application/octet-stream," + encodeURIComponent(reader.result));
+      return $('#download-link').show();
+    };
+    return reader.onerror = function(ev) {
+      return alert('error');
+    };
+  };
+
+  outPut = function(ev) {
+    var file, reader;
+    file = ev.target.files[0];
+    alert(file.name + ' is selected!');
+    reader = new FileReader();
+    reader.readAsText(file);
+    return reader.onload = function(ev) {
+      return this.attr('href', "data:application/octet-stream," + encodeURIComponent(reader.result));
+    };
+  };
+
+  /*
+  # event
+  */
+
+  $(function() {
+    $(document).on('change', '#selectFile', selectFile);
+    return $(document).on('click', '#download-link', outPut);
+  });
 
 }).call(this);
