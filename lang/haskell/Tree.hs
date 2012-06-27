6cb3af043613466d061584ffcbf2793a9644bb88
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2012-06-27 23:25:10 +0900

    hogfe

--- lang/haskell/Tree.hs
+++ lang/haskell/Tree.hs
@@ -7,33 +7,25 @@ import Directory
 import Data.Tree
 import System.FilePath
 
-target_dirs :: [FilePath]
-target_dirs = ["/Users/ent-imac/projects/misc/lang/haskell/", "/Users/ent-imac/projects/moapps_addon/"]
-
-filename :: String
-filename = "project.filelist.test"
-
 main :: IO ()
 main = do
   args <- getArgs
   let path = args!!0
-  putStrDirTree path
+  printTree path
 
-putStrDirTree :: FilePath -> IO ()
-putStrDirTree path = do
---   putStrLn parent
---   putStrLn path
+printTree :: FilePath -> IO ()
+printTree path = do
   t <- tree1 path
   putStrLn $ drawTree t
+
+tree1 :: FilePath -> IO (Tree FilePath)
+tree1 fullPath = tree parent path
+  where (parent, path) = splitFileName fullPath
 
 -- 再帰的にディレクトリツリーを扱う
 --
 -- http://codereview.stackexchange.com/questions/8431/how-can-i-make-this-recursive-directory-tree-printer-i-wrote-in-haskell-more-idi
 --
-tree1 :: FilePath -> IO (Tree FilePath)
-tree1 fullPath = tree parent path
-  where (parent, path) = splitFileName fullPath
-
 tree :: FilePath -> FilePath -> IO (Tree FilePath)
 tree parent path = do
   let fullPath = parent </> path
@@ -46,12 +38,3 @@ tree parent path = do
 -- getDirectoryContentsとかの型がFilePath -> IO [FilePath]だからtreeもIOを返すようになってるのか、、、
 -- => そうか入出力だからIOなのか
 
--- dirTree :: FilePath -> IO (Tree FilePath)
--- dirTree root = unfoldTreeM step (root,root)
---     where step (f,c) = do
---             fs <- getDirectoryContents f
---             ds <- filterM doesDirectoryExist fs
---             return (c, [(f </> d, d) | d <- ds, d /= "." && d /= ".."])
--- こっちはうまく行かない気がする
--- *Main> dirTree ".."
--- Node {rootLabel = "..", subForest = []}
