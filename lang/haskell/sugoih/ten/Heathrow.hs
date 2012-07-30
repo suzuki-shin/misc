39c47cb2c3e2abf7b8b1243e0ed84b56b7b55c44
Shinichiro Suzuki <shinichiro.su@gmail.com>
2012-07-30 20:19:30 +0900

    sugoih

--- lang/haskell/sugoih/ten/Heathrow.hs
+++ lang/haskell/sugoih/ten/Heathrow.hs
@@ -1,14 +1,15 @@
 {-# OPTIONS -Wall #-}
+import Data.List
 
 data Section = Section { getA :: Int, getB :: Int, getC :: Int } deriving (Show)
 type RoadSystem = [Section]
 
-heathrowToLondon :: RoadSystem
-heathrowToLondon = [ Section 50 10 30
-                   , Section 5 90 20
-                   , Section 40 2 25
-                   , Section 10 8 0
-                   ]
+-- heathrowToLondon :: RoadSystem
+-- heathrowToLondon = [ Section 50 10 30
+--                    , Section 5 90 20
+--                    , Section 40 2 25
+--                    , Section 10 8 0
+--                    ]
 
 data Label = A | B | C deriving (Show)
 type Path = [(Label, Int)]
@@ -42,3 +43,20 @@ roadStep (pathA, pathB) (Section a b c) =
                      else (C, c):(A, a):pathA
   in (newPathToA, newPathToB)
 
+
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf 0 _ = undefined
+groupsOf _ [] = []
+groupsOf n xs = take n xs : groupsOf n (drop n xs)
+
+
+main :: IO ()
+main = do
+  contents <- getContents
+  let threes = groupsOf 3 (map read $ lines contents)
+      roadSystem = map (\[a,b,c] -> Section a b c) threes
+      path = optimalPath roadSystem
+      pathString = concat $ map (show . fst) path
+      pathTime = sum $ map snd path
+  putStrLn $ "The best path to take is: " ++ pathString
+  putStrLn $ "Time taken: " ++ show pathTime
