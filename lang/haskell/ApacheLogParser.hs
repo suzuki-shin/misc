db698bde0ad8600cee990a7cae6d162684baf110
Shinichiro Suzuki <shinichiro.su@gmail.com>
2012-08-17 17:38:09 +0900

    対応するlogのフォーマット変えた

--- lang/haskell/ApacheLogParser.hs
+++ lang/haskell/ApacheLogParser.hs
@@ -6,7 +6,9 @@ module ApacheLogParser where
 
 import Text.Parsec
 import Text.Parsec.String
+import Data.List
 
+-- "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\" - - \"%{X-DCMGUID}i\" \"%{X-Up-Subno}i\" \"%{x-jphone-uid}i\" \"%{MOAPPS_CARRIER}e\" \"%{sp_uk}C\" \"%{sp_sess}C\" \"%Tsec\""
 data LogLine = LogLine {
     getIP :: String
   , getIdent :: String
@@ -17,6 +19,13 @@ data LogLine = LogLine {
   , getBytes :: String
   , getRef :: String
   , getUA :: String
+  , getDCMGUID :: String
+  , getUpSubno :: String
+  , getJphoneUid :: String
+  , getMoappsCarrier :: String
+  , getSpUk :: String
+  , getSpSecc :: String
+  , getSec :: String
   } deriving (Ord, Eq, Show)
 
 plainValue :: Parser String
@@ -55,7 +64,25 @@ logLine = do
   ref <- quotedValue
   space
   ua <- quotedValue
-  return $ LogLine ip ident user date req status bytes ref ua
+  space
+  char '-'
+  space
+  char '-'
+  space
+  dcmguid <- quotedValue
+  space
+  upsubno <- quotedValue
+  space
+  jphoneuid <- quotedValue
+  space
+  moappscarrier <- quotedValue
+  space
+  spuk <- quotedValue
+  space
+  spsecc <- quotedValue
+  space
+  sec <- quotedValue
+  return $ LogLine ip ident user date req status bytes ref ua dcmguid upsubno jphoneuid moappscarrier spuk spsecc sec
 
 logLines :: Parser [LogLine]
 logLines = endBy1 logLine eol
@@ -65,7 +92,15 @@ eol =     try (string "\n\r")
       <|> string "\n"
       <|> string "\r"
 
-testLine = "192.168.1.80 - - [18/Feb/2011:20:21:30 +0100] \"GET / HTTP/1.0\" 503 2682 \"-\" \"-\""
+
+uniqueUAList :: [LogLine] -> [String]
+uniqueUAList = map (\uaList -> uaList!!0) . group . sort . map (\log -> getUA log)
+
+uniqueElementList :: (LogLine -> String) ->  [LogLine] -> [String]
+uniqueElementList elem = map (\uaList -> uaList!!0) . group . sort . map (\log -> elem log)
+
+
+-- testLine = "192.168.1.80 - - [18/Feb/2011:20:21:30 +0100] \"GET / HTTP/1.0\" 503 2682 \"-\" \"-\""
 
 -- main = case parse logLine "(test)" testLine of
 --   Left err -> print err
