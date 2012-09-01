4a72f00e004d1f5ca652a3b93529d50f3539edbc
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2012-09-01 15:19:46 +0900

    post sample

---
+++ lang/haskell/http.hs
@@ -0,0 +1,23 @@
+import Control.Monad.IO.Class (liftIO)
+-- import Data.ByteString
+-- import Data.Conduit
+-- import qualified Data.Conduit.Binary as CB
+-- import Network.HTTP.Conduit
+-- import Web.Authenticate.OAuth
+import Network.HTTP
+
+import Network.URI
+import Data.Maybe
+
+test :: String -> String ->IO ()
+test url body = do
+  res <- simpleHTTP req
+  case res of
+    Left x -> return ()
+    Right r -> return ()
+  where req = Request {
+          rqURI = fromJust $ parseURI url,
+          rqMethod = POST,
+          rqHeaders = [ mkHeader HdrContentType "application/x-www-form-urlencoded" ,
+                        mkHeader HdrContentLength (show $ Prelude.length body) ],
+          rqBody = body}
