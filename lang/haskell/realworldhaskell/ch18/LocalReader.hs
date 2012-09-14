46eb4358271d626b4f653afdf1c83cb640eb467c
Shinichiro Suzuki <shinichiro.su@gmail.com>
2012-09-14 15:59:18 +0900

    sugoih chap 13 & othrers

---
+++ lang/haskell/realworldhaskell/ch18/LocalReader.hs
@@ -0,0 +1,16 @@
+{-# OPTIONS -Wall #-}
+
+import Control.Monad.Reader
+
+myName step = do
+  name <- ask
+  return (step ++ ", I am " ++ name)
+
+localExample :: Reader String (String, String, String)
+localExample = do
+  a <- myName "First"
+  b <- local (++"dy") (myName "Second")
+  c <- myName "Third"
+  return (a, b, c)
+
+
