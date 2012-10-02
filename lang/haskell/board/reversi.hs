e294f7683fa67a0f06e2627f60c9eb97d2e09c1c
Shinichiro Suzuki <shinichiro.su@gmail.com>
2012-10-02 22:42:42 +0900

    作りかけ

--- lang/haskell/board/reversi.hs
+++ lang/haskell/board/reversi.hs
@@ -2,7 +2,7 @@
 import Board
 import qualified Data.Map as M
 import Data.Maybe
-import Debug.Trace
+-- import Debug.Trace
 
 boardSize :: Int
 boardSize = 4
@@ -11,17 +11,35 @@ data Direct = LeftUpD | UpD | RightUpD
             | LeftD | RightD
             | LeftDownD | DownD | RightDownD
             deriving (Show, Eq)
-direct :: Pos -> Direct -> Pos
-direct (x, y) dir
+
+-- | 指定PosのDirect方向の隣のPosを返す
+-- >>> neighbor (1,1) LeftDownD
+-- (0,2)
+-- >>> neighbor (1,1) UpD
+-- (1,0)
+-- >>> neighbor (1,1) RightUpD
+-- (2,0)
+-- >>> neighbor (1,1) LeftD
+-- (0,1)
+-- >>> neighbor (1,1) RightD
+-- (2,1)
+-- >>> neighbor (1,1) LeftDownD
+-- (0,2)
+-- >>> neighbor (1,1) DownD
+-- (1,2)
+-- >>> neighbor (1,1) RightDownD
+-- (2,2)
+neighbor :: Pos -> Direct -> Pos
+neighbor (x, y) dir
   | dir == LeftUpD    = (x - 1, y - 1)
   | dir == UpD        = (x, y - 1)
   | dir == RightUpD   = (x + 1, y - 1)
   | dir == LeftD      = (x - 1, y)
   | dir == RightD     = (x + 1, y)
   | dir == LeftDownD  = (x - 1, y + 1)
-  | dir == DownD      = (x, y - 1)
+  | dir == DownD      = (x, y + 1)
   | dir == RightDownD = (x + 1, y + 1)
-direct _ _ = error "bad args"
+neighbor _ _ = error "bad args"
 
 allDirections :: [Direct]
 allDirections = [ LeftUpD, UpD , RightUpD
@@ -52,41 +70,37 @@ checkDraw board mark = isFinished board && length (marksPosOf board mark) == len
 -- []
 lineOfDirection :: BoardInfo -> Pos -> Direct -> [(Pos, Mark)]
 lineOfDirection boardInfo pos dir
-  |    isOnBoard (getSize boardInfo) pos == False
-    || isOnBoard (getSize boardInfo) nextPos == False
-    || isNothing markOfNextPos = []
+  | not (isOnBoard (getSize boardInfo) pos) || not (isOnBoard (getSize boardInfo) nextPos) || isNothing markOfNextPos = []
   | otherwise = (nextPos, fromJust markOfNextPos) : (lineOfDirection boardInfo nextPos dir)
   where
-    nextPos = direct pos dir
+    nextPos = neighbor pos dir
     markOfNextPos = markOf (getBoard boardInfo) nextPos
 
 -- | その(line::[(Pos,Mark)])は指定したmarkで挟めるlineか？はさめるならそのlineを、はさめないなら[]を返す
-isClipableLine :: Mark -> [(Pos, Mark)] -> [(Pos, Mark)]
-isClipableLine mark line = let revLine = takeWhile (\(_, m) -> m == (rev mark)) line
-                               lastPosMark = line!!(length revLine)
-                               -- | 逆markのline
-                               clipableLine :: Mark -> [(Pos, Mark)] -> [(Pos, Mark)]
-                               clipableLine mark line = revLine ++ [lastPosMark]
-                           in case (snd lastPosMark) /= mark of
-                             False -> []
-                             True -> clipableLine mark line
+-- >>> clippableLine O [((2,1),X),((3,1),X),((4,1),O)]
+-- [(2,1),(3,1)]
+-- >>> clippableLine O [((2,1),O),((3,1),X),((4,1),O)]
+-- []
+-- >>> clippableLine O [((2,1),X),((3,1),X),((4,1),E)]
+-- []
+clippableLine :: Mark -> [(Pos, Mark)] -> [Pos]
+clippableLine mark posMarks
+  | length body < length posMarks && snd (posMarks!!(length body)) == mark = body
+  | otherwise = []
+  where
+    body = body' mark posMarks
+    body' mark' (pm:pms)
+      | snd pm == rev mark' = (fst pm) : (body' mark' pms)
+      | otherwise = []
+    body' _ _ = []
 
 -- | そこにmarkを置いた場合あいてのmarkを挟めるか
+-- >>> let bSize = 4 :: Int
+-- >>> let bi = BoardInfo bSize $ M.insert (3,3) O $ M.insert (3,2) X $ M.insert (2,3) X $ M.insert (2,2) O (getBoard (emptyBoard bSize))
 canClip :: BoardInfo -> Pos -> Mark -> Bool
-canClip _ _ _ = undefined
--- canClip boardInfo pos mark = map (lineOfDirection boardInfo pos) allDirections
-
--- canClip board pos mark = case byEnemySide board pos mark of
---   Just dir -> canClip' board dir pos mark
---     where
---       -- 指定した方向の隣のMarkはmarkか、もしくはそこは逆のmarkでその向こうが自分のmarkか
---       canClip' :: Board -> Direct -> Pos -> Mark -> Bool
---       canClip' board' dir' pos' mark'
---         | markOf board' (direct pos' dir') == Just mark' = True
---         |    (markOf board' (direct pos' dir') == Just (rev mark'))
---           && canClip' board' dir' (direct pos' dir') mark' = True
---         | otherwise = False
---   Nothing -> False
+canClip bi p m
+  | filter (\d -> clippableLine m (lineOfDirection bi p d) /= []) allDirections == [] = False
+  | otherwise = True
 
 -- | となりに逆のマークがあるか？
 -- >>> import Data.Map
@@ -105,18 +119,18 @@ canClip _ _ _ = undefined
 -- Just RightDownD
 byEnemySide :: Board -> Pos -> Mark -> Maybe Direct
 byEnemySide board pos mark
-  | markOf board (direct pos RightD) == Just (rev mark)     = Just RightD
-  | markOf board (direct pos LeftD) == Just (rev mark)      = Just LeftD
-  | markOf board (direct pos DownD) == Just (rev mark)      = Just DownD
-  | markOf board (direct pos UpD) == Just (rev mark)        = Just UpD
-  | markOf board (direct pos RightDownD) == Just (rev mark) = Just RightDownD
-  | markOf board (direct pos LeftDownD) == Just (rev mark)  = Just LeftDownD
-  | markOf board (direct pos RightUpD) == Just (rev mark)   = Just RightUpD
-  | markOf board (direct pos LeftUpD) == Just (rev mark)    = Just LeftUpD
+  | markOf board (neighbor pos RightD) == Just (rev mark)     = Just RightD
+  | markOf board (neighbor pos LeftD) == Just (rev mark)      = Just LeftD
+  | markOf board (neighbor pos DownD) == Just (rev mark)      = Just DownD
+  | markOf board (neighbor pos UpD) == Just (rev mark)        = Just UpD
+  | markOf board (neighbor pos RightDownD) == Just (rev mark) = Just RightDownD
+  | markOf board (neighbor pos LeftDownD) == Just (rev mark)  = Just LeftDownD
+  | markOf board (neighbor pos RightUpD) == Just (rev mark)   = Just RightUpD
+  | markOf board (neighbor pos LeftUpD) == Just (rev mark)    = Just LeftUpD
   | otherwise = Nothing
 
 -- | そこにそのマークを置けるかどうか
--- >>> let bi = initBoard 4
+-- >>> let bi = initBoard 4 [((2,2),O), ((2,3),X),((3,2),X), ((3,3),O)]
 -- >>> canPut bi (1,3) O
 -- True
 -- >>> canPut bi (1,2) X
@@ -127,13 +141,20 @@ canPut :: BoardInfo -> Pos -> Mark -> Bool
 -- canPut boardInfo pos mark  =    trace("is not OnBoard") ((isOnBoard (getSize boardInfo) pos))
 --                              && trace("not E") (((markOf (getBoard boardInfo) pos) == Just E))
 --                              && trace("can not clip") (canClip boardInfo pos mark)
-canPut boardInfo pos mark  =    (isOnBoard (getSize boardInfo) pos)
-                             && ((markOf (getBoard boardInfo) pos) == Just E)
-                             && trace "### can not clip ###" (canClip boardInfo pos mark)
+canPut boardInfo pos mark  =    isOnBoard (getSize boardInfo) pos
+                             && (markOf (getBoard boardInfo) pos) == Just E
+                             && canClip boardInfo pos mark
+
+turnBack :: Board -> [Pos] -> Board
+turnBack board ps = foldl (\b p -> M.insert p (revMark p) b) board ps
+  where
+    revMark :: Pos -> Mark
+    revMark p = case M.lookup p board of
+      Just (pos', mark') -> mark'
+      Nothing -> 
 
 -- | markをおいたあとにboardInfoがどのように変更されるか
 action :: BoardInfo -> Pos -> Mark  -> BoardInfo
--- action boardInfo pos mark = BoardInfo (getSize boardInfo) (M.insert (4, 4) X (getBoard boardInfo))
 action boardInfo _ _ = BoardInfo (getSize boardInfo) (M.insert (4, 4) X (getBoard boardInfo))
 
 initBoard :: Int -> [(Pos, Mark)] -> BoardInfo
