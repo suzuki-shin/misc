e86413be43a6c0ac533f8c00a983200edbc5eaae
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2012-11-05 23:39:31 +0900

    matrixがすごい短くなった

--- lang/haskell/matrix.hs
+++ lang/haskell/matrix.hs
@@ -1,34 +1,37 @@
 {-# OPTIONS -Wall #-}
 import Control.Applicative
+import Data.List
 
 data State = O | X deriving Show
-type Matrix = [[(String, State)]]
 
 main :: IO ()
 main = do
    items <- lines <$> getContents
+   printHeader items
    printMatrix $ matrix items
    return ()
 
 -- | 入力項目のリストに対するO、Xのマトリクスを返す
 -- >>> matrix ["hoge","fuga","foo"]
--- [[("hoge",O),("fuga",O),("foo",O)],[("hoge",O),("fuga",O),("foo",X)],[("hoge",O),("fuga",X),("foo",O)],[("hoge",O),("fuga",X),("foo",X)],[("hoge",X),("fuga",O),("foo",O)],[("hoge",X),("fuga",O),("foo",X)],[("hoge",X),("fuga",X),("foo",O)],[("hoge",X),("fuga",X),("foo",X)]]
+-- [[O,O,O],[O,O,X],[O,X,O],[O,X,X],[X,O,O],[X,O,X],[X,X,O],[X,X,X]]
 -- >>> matrix []
 -- [[]]
-matrix :: [String] -> Matrix
-matrix items = mapM (\x -> [fst x, snd x]) $ map (\i -> ((i,O),(i,X))) items
+matrix :: [String] -> [[State]]
+matrix = mapM (\_ -> [O, X])
 
 -- | Matrixデータを表形式で出力する
-printMatrix :: Matrix -> IO ()
-printMatrix mtx = do
-  printHeader
-  printMatrix' mtx
-  where
-    printHeader = do
-      mapM_ (putStr . (++"\t") . fst) $ head mtx
-      putStrLn ""
-    printMatrix' (l:ls) = do
-      mapM_ (putStr . (++"\t") . show . snd) l
-      putStrLn ""
-      printMatrix' ls
-    printMatrix' [] = return ()
+printMatrix :: [[State]] -> IO ()
+printMatrix (col:cols) = do
+  mapM_ putStr $ listToTsv $ map show col
+  putStrLn ""
+  printMatrix cols
+printMatrix [] = return ()
+
+-- | ヘッダを出力する
+printHeader :: [String] -> IO ()
+printHeader items = do
+  mapM_ putStr $ listToTsv items
+  putStrLn ""
+
+listToTsv :: [String] -> [String]
+listToTsv items = intersperse "\t" items
