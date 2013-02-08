56a39ae92b2a30d9306bdca70b259447c2775032
Shinichiro Suzuki <shinichiro.su@gmail.com>
2013-02-08 13:50:10 +0900

    現在開いているタブをsuggestに入れれた

--- lang/javascript/chrome_ext/experiment/background.js
+++ lang/javascript/chrome_ext/experiment/background.js
@@ -1,19 +1,44 @@
+var Background;
+Background = (function(){
+  Background.displayName = 'Background';
+  var prototype = Background.prototype, constructor = Background;
+  function Background(){}
+  return Background;
+}());
+Background.suggestList = [];
+chrome.omnibox.onInputStarted.addListener(function(){
+  var listTabs;
+  console.log('inputStarted:');
+  listTabs = function(tabs){
+    var list, res$, i$, len$, t;
+    console.log('listTabs');
+    res$ = [];
+    for (i$ = 0, len$ = tabs.length; i$ < len$; ++i$) {
+      t = tabs[i$];
+      res$.push({
+        content: t.title,
+        description: t.title
+      });
+    }
+    list = res$;
+    console.log(list);
+    return Background.suggestList = Background.suggestList.concat(list);
+  };
+  return chrome.tabs.query({
+    currentWindow: true
+  }, listTabs);
+});
 chrome.omnibox.onInputChanged.addListener(function(text, suggest){
   var list;
   console.log('inputChanged: ' + text);
-  list = [
-    {
-      content: text + " one!",
-      description: "the first 1"
-    }, {
-      content: text + " number two!!",
-      description: "the 2 entry"
-    }, {
-      content: text + " number three!!!",
-      description: "the 3 entry"
-    }
-  ];
-  return suggest(list);
+  list = [{
+    content: text + " one!",
+    description: "the first 1"
+  }];
+  console.log(list);
+  Background.suggestList = Background.suggestList.concat(list);
+  console.log(Background.suggestList);
+  return suggest(Background.suggestList);
 });
 chrome.omnibox.onInputEntered.addListener(function(text){
   console.log('inputEntered: ' + text);
--- lang/javascript/chrome_ext/experiment/background.ls
+++ lang/javascript/chrome_ext/experiment/background.ls
@@ -1,19 +1,30 @@
+class Background
+
+Background.suggestList = []
+
+chrome.omnibox.onInputStarted.addListener(->
+  console.log('inputStarted:')
+  listTabs = (tabs) ->
+    console.log('listTabs')
+    list = [{content: t.title, description: t.title} for t in tabs]
+    console.log(list)
+    Background.suggestList = Background.suggestList +++ list
+  chrome.tabs.query({currentWindow: true}, listTabs)
+)
+
 chrome.omnibox.onInputChanged.addListener(((text, suggest) ->
   console.log('inputChanged: ' + text)
 
-#   chrome.tabs.query({currentWindow: true}, listTabs)
-
-#   listTabs = (tabs) ->
-#     list = [{content: text, description: t.title} for t in tabs]
-#     console.log(list)
-#     suggest(list)
-
   list = [
-    {content: text + " one!", description: "the first 1"}
-    {content: text + " number two!!", description: "the 2 entry"}
-    {content: text + " number three!!!", description: "the 3 entry"}
+#     {content: text + " one!", description: "the first 1"}
+#     {content: text + " number two!!", description: "the 2 entry"}
+#     {content: text + " number three!!!", description: "the 3 entry"}
   ]
-  suggest(list)
+  console.log(list)
+  Background.suggestList = Background.suggestList +++ list
+  console.log(Background.suggestList)
+  suggest(Background.suggestList)
+
 #   suggest([
 #     {content: text + " one!", description: "the first 1"}
 #     {content: text + " number two!!", description: "the 2 entry"}
