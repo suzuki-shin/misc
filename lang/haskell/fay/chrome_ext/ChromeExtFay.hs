d15b9192ffa2958f965db970c0798842c35b41e2
Shinichiro Suzuki <shinichiro.su@gmail.com>
2013-02-28 20:08:12 +0900

    moveNextCursorとかを作るところから

--- lang/haskell/fay/chrome_ext/ChromeExtFay.hs
+++ lang/haskell/fay/chrome_ext/ChromeExtFay.hs
@@ -103,10 +103,6 @@ defaultSettings = [
   ]
 
 keyMap :: [(Int, String)]
--- keyMap = [
---   (1, "aa"),
---   (2, "bb")
---   ]
 keyMap = [
   (9   , "TAB"),
   (16  , "SHIFT"),
@@ -253,60 +249,12 @@ isHitAHintKey keyCode = elem keyCode $ map fst _hintKeys
 
 data Item = Item { getId :: String, getTitle :: String, getUrl :: String, getType :: String } deriving (Show)
 
--- # (tab|history|bookmark|,,,)のリストをうけとりそれをhtmlにしてappendする
-makeSelectorConsole :: [Item] -> Fay JQuery
-makeSelectorConsole items = do
+makeSelectorConsole :: String -> Fay JQuery
+makeSelectorConsole htmlStr = do
   putStrLn "makeSelectorConsole"
-  putStrLn $ show $ length items
-  putStrLn $ show $ (items!!0)  -- OK {"getId":4,"getTitle":"拡張機能","getUrl":"chrome://extensions/","getType":"tab"}
-  putStrLn $ show $ getTitle (items!!0) -- OK "拡張機能"
---   putStrLn (["<tr id=\"" ++ getType t ++ "-" ++ getId t ++ "\"><td><span class=\"title\">["++ getType t ++ "] " ++ getTitle t ++ " </span><span class=\"url\"> " ++ getUrl t ++ "</span></td></tr>" | t <- items]!!0)
-  putStrLn "--- 0"
-  putStrLn $ "aaa" ++ "bbb"     -- OK "aaabbb"
-  let a = getTitle (items!!0)
-      b = getType (items!!0)
-  putStrLn a                    -- NG なにも出力されない
-  putStrLn b                    -- NG なにも出力されない
-  putStrLn $ show a ++ show b   -- OK 
---   let a = show $ getTitle (items!!0)
---       b = show $ getType (items!!0)
---   putStrLn a
---   putStrLn b
---   putStrLn $ a ++ b
-  putStrLn $ show $ head $ map getTitle items    -- OK "拡張機能"
-  putStrLn $ show $ [getTitle t | t <- items]!!0 -- OK "拡張機能"
-  putStrLn $ [getTitle t | t <- items]!!0 -- NG なにも出力されないみたい
---   putStrLn $ show $ [concat [getTitle t, " ", getUrl t] | t <- items]!!0 -- NG Error in event handler for 'undefined': undefined undefined 
---   putStrLn $ show $ [getTitle t ++ " " ++ getUrl t | t <- items]!!0 -- NG Error in event handler for 'undefined': undefined undefined 
-  putStrLn $ show $ (trs items)!!0               -- NG {"car":"<","cdr":{"forced":false}}
-  putStrLn "--- 1"
-  putStrLn $ show $ ts items
-  putStrLn $ show $ ts' items   -- NG {"car":"#","cdr":{"forced":false}}
-
-  putStrLn "--- 2"
-  hoge <- arrToStr $ ts items
-  putStrLn hoge
---   putStrLn $ show $ concat $ map (\t -> getType t ++ "-" ++ getId t) $ take 10 items
---   putStrLn $ show $ concat $ take 10 [getType t ++ "-" ++ getId t| t <- items]
---   putStrLn $ show $ head $ map getTitle items
   select "#selectorList" >>= remove
-  select "#selectorConsole" >>= append hoge
---   select "#selectorConsole" >>= append "ABCDEFG"
+  select "#selectorConsole" >>= append htmlStr
   select "#selectorList tr:first" >>= addClass "selected"
-  where
-    num = 20
---     trs = map (\t -> getTitle t) items
---     trs = map (\t -> "<tr id=\"" ++ getType t ++ "-" ++ getId t ++ "\"><td><span class=\"title\">["++ getType t ++ "] " ++ getTitle t ++ " </span><span class=\"url\"> " ++ getUrl t ++ "</span></td></tr>") items
-    trs :: [Item] -> [String]
-    trs items = ["<tr id=\"" ++ show (getType t) ++ "-" ++ show (getId t) ++ "\"><td><span class=\"title\">[" ++ show (getType t) ++ "] " ++ show (getTitle t) ++ " </span><span class=\"url\"> " ++ show (getUrl t) ++ "</span></td></tr>" | t <- items]
-    ts :: [Item] -> String
-    ts items = "<table id=\"selectorList\">" ++ concat (trs (take 10 items)) ++ "</table>"
-    trs' :: [Item] -> [String]
-    trs' items = ["###<tr id=\"" ++ getType t ++ "-" ++ getId t ++ "\"><td><span class=\"title\">[" ++ getType t ++ "] " ++ getTitle t ++ " </span><span class=\"url\"> " ++ getUrl t ++ "</span></td></tr>" | t <- items]
-    ts' :: [Item] -> String
-    ts' items = (trs' items)!!5
---     ts' items = concat (trs' items)
---     ts' items = concat (trs' (take 10 items))
 
 remove :: JQuery -> Fay JQuery
 remove = ffi "%1.remove()"
@@ -342,10 +290,10 @@ keyupMap e (St modeRef ctrlRef altRef _ _) = do
     17 -> do
       putStrLn "ctrlKeyCode"
       writeRef ctrlRef False
-    18  -> do
+    18 -> do
       putStrLn "altKeyCode"
       writeRef altRef False
-    _           -> do
+    _ -> do
       mode <- readRef modeRef
       putStrLn $ show mode
       keyupMap' e mode ctrlRef altRef
@@ -379,10 +327,10 @@ keydownMap e (St modeRef ctrlRef altRef inputIdxRef _) = do
     17 -> do
       putStrLn "ctrlKeyCode"
       writeRef ctrlRef True
-    18  -> do
+    18 -> do
       putStrLn "altKeyCode"
       writeRef altRef True
-    _           -> do
+    _ -> do
       mode <- readRef modeRef
       putStrLn $ show mode
       keydownMap' e mode ctrlRef altRef
@@ -412,6 +360,14 @@ keydownMap e (St modeRef ctrlRef altRef inputIdxRef _) = do
         _ -> if isHitAHintKey (getKeyCode e)
              then hitHintKey e
              else return ()
+    keydownMap' e SelectorMode ctrlRef altRef = do
+      ctrl <- readRef ctrlRef
+      alt <- readRef altRef
+      case keyMapper (Key (getKeyCode e) ctrl alt) defaultSettings of
+        Just "MOVE_NEXT_SELECTOR_CURSOR" -> moveNextCursor e
+        Just "MOVE_PREV_SELECTOR_CURSOR" -> movePrevCursor e
+        Just "CANCEL" -> cancel e
+        _ -> return ()
     keydownMap' _ _ _ _ = return ()
 
 keyup :: (Event -> Fay ()) -> Fay ()
@@ -432,9 +388,6 @@ startHah modeRef = do
         Just hintKeyName -> "<div class=\"hintKey\">" ++ hintKeyName ++ "</div> " ++ oldHtml
         _ -> oldHtml
       _ -> oldHtml
---     f (i, oldHtml) = case lookup (indexToKeyCode i) hintKeys of
---       Just hintKeyName -> "<div class=\"hintKey\">" + hintKeyName + "</div> " ++ oldHtml
---       _ -> oldHtml
 
 focusForm :: Ref Mode -> Ref Int -> Fay ()
 focusForm modeRef inputIdxRef = do
@@ -459,9 +412,27 @@ filterSelector keyCode = do
   putStrLn "filterSelector"
 
 focusNextForm = undefined
+-- focusNextForm :: Event -> Ref Int -> Fay ()
+-- focusNextForm e inputIdxRef = do
+--   preventDefault e
+--   idx <- readRef inputIdxRef
+--   writeRef inputIdxRef $ idx + 1
+
+--       e.preventDefault()
+--       console.log('focusNextForm')
+--       Main.formInputFieldIndex += 1
+--       console.log(Main.formInputFieldIndex)
+--       console.log($(FORM_INPUT_FIELDS))
+--       console.log($(FORM_INPUT_FIELDS).eq(Main.formInputFieldIndex))
+--       if $(FORM_INPUT_FIELDS).eq(Main.formInputFieldIndex)?
+--         $(FORM_INPUT_FIELDS).eq(Main.formInputFieldIndex).focus()
+
+
 focusPrevForm = undefined
 cancel = undefined
 hitHintKey = undefined
+moveNextCursor = undefined
+movePrevCursor = undefined
 
 jqHtml :: ((Int, String) -> a) -> JQuery -> Fay JQuery
 jqHtml = ffi "%2.html(f)"
@@ -507,18 +478,11 @@ start = do
 
   chromeExtensionSendMessage "{\"mes\": \"makeSelectorConsole\"}" $ \is -> do
     putStrLn "extension.sendMessage"
---     putStrLn $ show (is!!0)
-    putStrLn $ show is
-    putStrLn "---"
---     putStrLn is
     items <- fromJSON $ show is
-    putStrLn $ show $ items!!0
---     putStrLn $ show $ (\x -> Item getId x getTitle x getUrl x getType x) (items!!0)
---     items <- toItemsFromJSON $ show is
     writeRef listRef items
     select "body" >>= append "<div id=\"selectorConsole\"><form id=\"selectorForm\"><input id=\"selectorInput\" type=\"text\" /></form></div>"
-    makeSelectorConsole items
---     makeSelectorConsole $ map (\i -> Item { getId = (id i), getTitle = (title i), getUrl = (url i), getType = (type i)) is
+    items' <- arrToStr $ "<table id=\"selectorList\">" ++ concat ["<tr id=\"" ++ show (getType t) ++ "-" ++ show (getId t) ++ "\"><td><span class=\"title\">[" ++ show (getType t) ++ "] " ++ show (getTitle t) ++ " </span><span class=\"url\"> " ++ show (getUrl t) ++ "</span></td></tr>" | t <- (take 10 items)] ++ "</table>"
+    makeSelectorConsole items'
     return ()
 
 --   isFocusingForm then writeRef modeRef FormFocusMode else Fay ()
@@ -535,7 +499,6 @@ decideSelector e = do
 preventDefault :: Event -> Fay ()
 preventDefault = ffi "%1.preventDefault()"
 
-
 data Ref a
 
 newRef :: a -> Fay (Ref a)
@@ -550,19 +513,8 @@ readRef = ffi "Fay$$readRef(%1)"
 chromeExtensionSendMessage :: String -> (a -> Fay ()) -> Fay ()
 chromeExtensionSendMessage = ffi "chrome.extension.sendMessage(JSON.parse(%1), %2)"
 
--- { getId :: String, getTitle :: String, getUrl :: String, getType :: String }
--- getItemFromJSON :: a -> (String, String, String, String)
--- getItemFromJSON = ffi "%1.id,
-
--- jsonParse :: String -> Fay a
--- jsonParse = ffi "JSON.stringify(%1)"
-
 fromJSON :: String -> Fay [Item]
 fromJSON = ffi "JSON.parse(%1)"
 
 arrToStr :: [Char] -> Fay String
 arrToStr = ffi "%1.join('')"
-
-{--
-
---}
