f67a3c8b0f0b2dbef278f522f81464bdf55052c8
Shinichiro Suzuki <shinichiro.su@gmail.com>
2013-03-15 15:17:44 +0900

    48scheme再会

--- lang/haskell/48scheme/chapter2.hs
+++ lang/haskell/48scheme/chapter2.hs
@@ -1,6 +1,7 @@
 module Main where
 import Text.ParserCombinators.Parsec hiding (spaces)
-import System
+import System.Environment
+import Control.Monad
 
 main :: IO ()
 main = do args <- getArgs
@@ -10,7 +11,7 @@ symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
   Left err -> "No match:" ++ show err
   Right val -> "Found value"
 
@@ -29,3 +30,33 @@ parseString = do char '"'
                  x <- many (noneOf "\"")
                  char '"'
                  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first:rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _ -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+-- parseNumber = do
+--   d <- many1 digit
+--   return $ (Number . read) d
+-- parseNumber = many1 digit >>= (\d -> return $ (Number . read) d)
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom <|> parseString <|> parseNumber
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+  head <- endBy parseExpr spaces
+  tail <- char '.' >> spaces >> parseExpr
+  return $ DottedList head tail
+
