c0cc530ec96d4ddb0ca4659008cea2f1196a1519
Shinichiro Suzuki <shinichiro.su@gmail.com>
2013-03-22 19:13:41 +0900

    isIn

--- lang/haskell/MyUtil/MyUtil.hs
+++ lang/haskell/MyUtil/MyUtil.hs
@@ -3,6 +3,7 @@ module MyUtil (
     replace
   , replaceAll
   , splitOn                     -- Data.List.Split.splitOn
+  , isIn
   ) where
 
 import Data.List
@@ -16,3 +17,18 @@ replace searchStr replaceStr  = intercalate replaceStr . splitOn searchStr
 replaceAll :: [(String, String)] -> String -> String
 replaceAll [] s = s
 replaceAll (m:ms) s = replaceAll ms (replace (fst m) (snd m) s)
+
+-- | あるリストの全ての要素が別のリストに含まれるかを返す(並び順は問わない)
+-- >>> [1,2,3] `isIn` [3,1,2,4,5]
+-- True
+-- >>> [1,2,3] `isIn` [1,2,4,5]
+-- False
+-- >>> [] `isIn` [1,2,4,5]
+-- True
+-- >>> [] `isIn` []
+-- True
+-- >>> [(1,2),(2,3)] `isIn` [(3,3),(1,2),(4,5),(2,3)]
+-- True
+isIn :: Eq a => [a] -> [a] -> Bool
+(x:xs) `isIn` ys = (x `elem` ys) && (xs `isIn` ys)
+[] `isIn` _ = True
