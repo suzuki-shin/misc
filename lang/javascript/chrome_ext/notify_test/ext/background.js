d4d672272278e6db9771d87645048d9107ebe99e
Shinichiro Suzuki <shinichiro.su@gmail.com>
2013-04-05 16:20:46 +0900

    hoge

--- lang/javascript/chrome_ext/notify_test/ext/background.js
+++ lang/javascript/chrome_ext/notify_test/ext/background.js
@@ -1,14 +1,26 @@
-var n;
-if (window.webkitNotifications) {
-  console.log("Notifications are supported!");
-  if (webkitNotifications.checkPermission() === 0) {
-    n = webkitNotifications.createNotification('icon.png', 'fugaq', 'messsssee');
-    n.show();
-    console.log('createNotification');
+var minToMilSec, startTimer, finishTimer;
+minToMilSec = function(minutes){
+  return minutes * 60 * 1000;
+};
+startTimer = function(minutes){
+  console.log('startTimer');
+  return setTimeout(finishTimer, minToMilSec(minutes));
+};
+finishTimer = function(){
+  var n;
+  console.log('finishTimer');
+  if (window.webkitNotifications) {
+    console.log("Notifications are supported!");
+    if (webkitNotifications.checkPermission() === 0) {
+      n = webkitNotifications.createNotification('icon.png', 'fugaq', 'time up!');
+      n.show();
+      return console.log('createNotification');
+    } else {
+      webkitNotifications.requestPermission();
+      return console.log('requestPermission');
+    }
   } else {
-    webkitNotifications.requestPermission();
-    console.log('requestPermission');
+    return console.log("Notifications are not supported for this Browser/OS version yet.");
   }
-} else {
-  console.log("Notifications are not supported for this Browser/OS version yet.");
-}
+};
+startTimer(1);
--- lang/javascript/chrome_ext/notify_test/src/background.ls
+++ lang/javascript/chrome_ext/notify_test/src/background.ls
@@ -1,11 +1,21 @@
-if window.webkitNotifications
-  console.log("Notifications are supported!")
-  if webkitNotifications.checkPermission() is 0
-    n = webkitNotifications.createNotification('icon.png', 'fugaq', 'messsssee')
-    n.show()
-    console.log('createNotification')
+minToMilSec = (minutes) -> minutes * 60 * 1000
+
+startTimer = (minutes) ->
+  console.log('startTimer')
+  setTimeout(finishTimer, minToMilSec(minutes))
+
+finishTimer =->
+  console.log('finishTimer')
+  if window.webkitNotifications
+    console.log("Notifications are supported!")
+    if webkitNotifications.checkPermission() is 0
+      n = webkitNotifications.createNotification('icon.png', 'fugaq', 'time up!')
+      n.show()
+      console.log('createNotification')
+    else
+      webkitNotifications.requestPermission()
+      console.log('requestPermission')
   else
-    webkitNotifications.requestPermission()
-    console.log('requestPermission')
-else
-  console.log("Notifications are not supported for this Browser/OS version yet.")
+    console.log("Notifications are not supported for this Browser/OS version yet.")
+
+startTimer(1)
