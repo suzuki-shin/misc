238129e43e9ca027f5916f2640603a1ca941028a
Shinichiro Suzuki <shinichiro.su@gmail.com>
2013-12-21 16:48:56 +0900

    Sも$にしちゃってたの直した

--- lang/haskell/maze/main.hs
+++ lang/haskell/maze/main.hs
@@ -1,29 +1,29 @@
 {-# OPTIONS_GHC -Wall #-}
 import Control.Monad (guard)
-import Control.Applicative ((<$>),(<*>))
 import Data.Array (Array, listArray, assocs, (!), elems, (//))
 
 
 data Direction = E | W | S | N deriving (Show, Eq)
 type Pos = (Int,Int) -- (y,x)
 type Tile = Char
-type MapData = Array Pos Tile
+-- type MapData = Array Pos Tile
+data MapData = MapData {mapData :: Array Pos Tile, height :: Int, width :: Int}
 
 -- | 読み込んだ文字列を2次元Arrayに変換する
 stringTo2DArray :: Int -> Int -> String -> MapData
-stringTo2DArray h w input = listArray ((0,0), (h,w)) $ filter (/='\n') input
+stringTo2DArray h w input = MapData (listArray ((0,0), (h,w)) $ filter (/='\n') input) h w
 
-printMapData :: Int -> MapData -> IO ()
-printMapData width m = do
+printMapData :: MapData -> IO ()
+printMapData (MapData m _ w) = do
   let mStr = elems m
-  printMapData' width mStr
+  printMapData' w mStr
   return ()
   where
     printMapData' :: Int -> String -> IO ()
     printMapData' _ "" = return ()
-    printMapData' width s = do
-      putStrLn $ take width s
-      printMapData' width $ drop width s
+    printMapData' w' s = do
+      putStrLn $ take w' s
+      printMapData' w' $ drop w' s
 
 -- | start位置座標を返す
 startPos :: MapData -> Pos
@@ -34,16 +34,16 @@ goalPos :: MapData -> Pos
 goalPos = posOf 'G'
 
 posOf :: Tile -> MapData -> Pos
-posOf tile ar = fst $ head $ filter (\(_,t) -> t == tile) $ assocs ar
+posOf tile (MapData ar _ _) = fst $ head $ filter (\(_,t) -> t == tile) $ assocs ar
 
 -- | 指定した位置から移動した次の位置のリストを返す (壁'*'には移動できない)
-nextPoss :: MapData -> Int -> Int -> Pos -> [Pos]
-nextPoss m height width p = do
+nextPoss :: MapData -> Pos -> [Pos]
+nextPoss (MapData m h w) p = do
   d <- [E,W,S,N]
   let p' = move d p
       y' = fst p'
       x' = snd p'
-  guard $ (y' >= 0) && (y' < height) && (x' >= 0) && (x' < width)
+  guard $ (y' >= 0) && (y' < h) && (x' >= 0) && (x' < w)
   guard $ (m ! p') /= '*'
   return p'
 
@@ -53,34 +53,25 @@ move W (y,x) = (y,x-1)
 move S (y,x) = (y+1,x)
 move N (y,x) = (y-1,x)
 
-searchRoute :: MapData -> Int -> Int -> [Pos] -> Pos -> [[Pos]]
-searchRoute m h w tracks currentP = do
-  if (m ! currentP) == 'G'
+searchRoute :: MapData -> [Pos] -> Pos -> [[Pos]]
+searchRoute m tracks currentP = do
+  if ((mapData m) ! currentP) == 'G'
      then return tracks
      else do
        if currentP `elem` tracks
          then []
          else do
-           nextP <- nextPoss m h w currentP
-           searchRoute m h w (currentP:tracks) nextP
+           nextP <- nextPoss m currentP
+           searchRoute m (currentP:tracks) nextP
 
 main :: IO ()
 main = do
   c <- getContents
-  let height = length $ lines c
-      width  = length $ head $ lines c
-      a = stringTo2DArray (height - 1) (width - 1) c
-      startP = startPos a
-      b = searchRoute a height width [] startP
-      minlen = minimum $ map length b
-      b0 = filter (\r -> length r == minlen) b
-      c0 = map (\p -> (p,'$')) $ head b0
-  print height
-  print width
-  print minlen
-  print $ startPos a
-  print $ goalPos a
-  if length b0 == 0
-    then print "no route"
-    else print $ head b0
-  printMapData width $ a // c0
+  let h = length $ lines c
+      w  = length $ head $ lines c
+      mData = stringTo2DArray (h - 1) (w - 1) c
+      routes = searchRoute mData [] $ startPos mData
+      minlen = minimum $ map length routes
+      minRoutes = filter (\r -> length r == minlen) routes
+      minRoute = if length minRoutes == 0 then [] else head minRoutes
+  printMapData $ MapData ((mapData mData) // init (map (\p -> (p,'$')) $ minRoute)) h w
