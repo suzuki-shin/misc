b2aa3d268539b7bb585cd47e4bd5df08b2b51b80
Shinichiro Suzuki <shinichiro.su@gmail.com>
2014-01-10 20:03:13 +0900

    misc

--- lang/haskell/stock2/Stock.hs
+++ lang/haskell/stock2/Stock.hs
@@ -18,10 +18,12 @@ import Data.Time.Calendar
 import Text.XML.HXT.Core
 import Text.XML.HXT.Curl
 import Control.Arrow
+import Control.Applicative ((<$>))
+import Control.Monad (forM_)
 
 connect = H.connectSqlite3
 
-data Company = Company {code :: Int, name :: String, mPrice :: Maybe Double} deriving (Show, Eq)
+data Company = Company {code :: Int, name :: String} deriving (Show, Eq)
 data Daily = Daily {
   companyCode :: Int
  ,adjustedPrice :: Double
@@ -34,8 +36,7 @@ data Daily = Daily {
 } deriving (Show, Eq)
 
 insertCompany :: IConnection conn => conn -> Stock.Company -> IO Integer
-insertCompany conn (Company code name Nothing) = H.run conn "insert into company (code, name) values (?,?);" [H.toSql code, H.toSql name]
-insertCompany conn (Company code name (Just price)) = H.run conn "insert into company (code, name, price) values (?,?,?);" [H.toSql code, H.toSql name, H.toSql price]
+insertCompany conn (Company code name) = H.run conn "insert into company (code, name) values (?,?);" [H.toSql code, H.toSql name]
 
 insertDaily :: IConnection conn => conn -> Stock.Daily -> IO Integer
 insertDaily conn (Daily companyCode adjustedPrice startPrice finishPrice highPrice lowPrice volume date)
@@ -46,11 +47,19 @@ hoge :: IO ()
 hoge = do
   conn <- H.connectSqlite3 "test1.db"
   -- H.run conn "insert into company (code, name) values (?,?);" [H.toSql (3::Int), H.toSql "xxhoge"]
-  insertCompany conn (Company 122 "あああ" (Just 235.5))
-  insertDaily conn (Daily 122 200.0 291.0 200.0 299.1 190.9 2000008 (fromGregorian 2014 1 9))
-  cs <- H.quickQuery' conn "select * from company;" []
-  cs <- H.quickQuery' conn "select * from daily;" []
-  print cs
+  -- insertCompany conn (Company 122 "あああ" (Just 235.5))
+  -- insertDaily conn (Daily 122 200.0 291.0 200.0 299.1 190.9 2000008 (fromGregorian 2014 1 9))
+  codes <- map ((H.fromSql::H.SqlValue -> Int) . head) <$> H.quickQuery' conn "select code from company;" []
+  -- cs <- H.quickQuery' conn "select * from daily;" []
+  -- mapM_ print codes
+  -- forM_ codes $ \code -> do
+  -- mapM_ (\code -> do
+  --   (_, st, hi, lo, fi, vol) <- getDaily_ (show code)
+  --   print $ Daily code (read st) (read fi) (read hi) (read lo) (read vol) (fromGregorian 2014 1 10)
+  --   ) codes
+  r <- getDaily_ $ (show . head) codes
+  print r
+  print codes
   H.commit conn
   H.disconnect conn
 
@@ -65,4 +74,9 @@ getDaily = atTag "daily" >>>
     hiPrice <- text <<< atTag "high_price"    -< vs
     loPrice <- text <<< atTag "low_price"     -< vs
     fiPrice <- text <<< atTag "closing_price" -< vs
-    returnA -< (date_, stPrice, hiPrice, loPrice, fiPrice)
+    volume  <- text <<< atTag "turnover" -< vs
+    returnA -< (date_, stPrice, hiPrice, loPrice, fiPrice, volume)
+
+getDaily_ code = runX (readDocument [withCurl []] url >>> getDaily)
+  where
+    url = "http://ikachi.sub.jp/kabuka/api/d/xml.php?stdate=20100104&eddate=20100107&code=" ++ code
